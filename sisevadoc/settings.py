8c8f263488d0e249e10639f9632d2587353579d2
Reynaldo Pereira Heredia <darkdeymon04@gmail.com>
2019-01-14 16:06:23 -0400

    v22e35

--- sisevadoc/settings.py
+++ sisevadoc/settings.py
@@ -85,9 +85,17 @@ WSGI_APPLICATION = 'sisevadoc.wsgi.application'
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
 DATABASES = {
+    #'default': {
+    #    'ENGINE': 'django.db.backends.sqlite3',
+    #    'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    #}
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'sisevadoc',                      
+        'USER': 'postgres',
+        'PASSWORD': '123456789',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
